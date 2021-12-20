ae825e58d74c0c3db30850c1269878604b8f6a0b
Kevin Ullbrandt <kevin@ullbrandt.com>
2021-12-20 13:41:31 +0100

    may crash

---
+++ .Net/MainShowCase/MainShowCase/Models/IProjectRepository.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+namespace MainShowCase.Models
+{
+    public interface IProjectRepository
+    {
+        public IEnumerable<Project> AllProjects { get; }
+    }
+}
---
+++ .Net/MainShowCase/MainShowCase/Models/MockProjectRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+namespace MainShowCase.Models
+{
+    public class MockProjectRepository : IProjectRepository
+    {
+        public IEnumerable<Project> AllProjects =>
+        new List<Project>
+        {
+        new Project{PeojectId = 1, Name = "Unknown", Description = "Unknown", ImageUrl="https://www.codecademy.com/resources/blog/content/images/2021/07/10-coding-projects-for-beginners.png", ProjectUrl="https://github.com/SERUSEcode?tab=repositories"}
+        };
+    }
+}
