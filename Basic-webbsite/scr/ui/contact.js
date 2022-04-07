950e187aac764221e9fe729e993be2f358501f9f
Kevin U <kevin@ullbrandt.com>
2022-04-07 17:25:30 +0200

    Added more style to the site

--- Basic-webbsite/scr/ui/contact.js
+++ Basic-webbsite/scr/ui/contact.js
@@ -4,9 +4,9 @@ document.getElementById("contactUrl").addEventListener("click", function() {
 
     main.innerHTML = `
     <p>
-        <a href="mailto:kevin@ullbrandt.com">
-            kevin@ullbrandt.com
-        </a>
+        <a href="mailto:kevin@ullbrandt.com"> kevin@ullbrandt.com</a>
+        <br>
+        <img id="profilePic" src="scr/img/KevinsFace.jpg" alt="" style="width: 40%; padding-top: 10%;">
     </p>
     `;
 });
