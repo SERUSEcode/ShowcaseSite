e53c2c7f6c9a0c3618bce25b05f38d9692a12569
Kevin U <kevin@ullbrandt.com>
2022-09-28 16:06:49 +0200

    pushing build for live server

---
+++ React/dist/index.js
@@ -0,0 +1,25 @@
+var $gXNCa$reactjsxruntime = require("react/jsx-runtime");
+require("react");
+var $gXNCa$reactdomclient = require("react-dom/client");
+
+function $parcel$interopDefault(a) {
+  return a && a.__esModule ? a.default : a;
+}
+
+
+
+const $86170abfc0122593$var$App = ()=>{
+    return /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)("h1", {
+        children: "Hall\xe5 d\xe4r v\xe4rlden"
+    });
+};
+var $86170abfc0122593$export$2e2bcd8739ae039 = $86170abfc0122593$var$App;
+
+
+
+
+const $4fa36e821943b400$var$root = (0, ($parcel$interopDefault($gXNCa$reactdomclient))).createRoot(document.getElementById("root"));
+$4fa36e821943b400$var$root.render(/*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)((0, $86170abfc0122593$export$2e2bcd8739ae039), {}));
+
+
+//# sourceMappingURL=index.js.map
