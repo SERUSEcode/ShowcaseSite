34d61fb1c420bf309ada40fa83e95324e4b48793
Kevin U <kevin@ullbrandt.com>
2022-09-28 14:35:42 +0200

    Fixed so I can run build

---
+++ React/public/index.js
@@ -0,0 +1,25 @@
+var $g36G4$reactjsxruntime = require("react/jsx-runtime");
+require("react");
+var $g36G4$reactdomclient = require("react-dom/client");
+
+function $parcel$interopDefault(a) {
+  return a && a.__esModule ? a.default : a;
+}
+
+
+
+const $86170abfc0122593$var$App = ()=>{
+    return /*#__PURE__*/ (0, $g36G4$reactjsxruntime.jsx)("h1", {
+        children: "Hall\xe5 d\xe4r v\xe4rlden"
+    });
+};
+var $86170abfc0122593$export$2e2bcd8739ae039 = $86170abfc0122593$var$App;
+
+
+
+
+const $4fa36e821943b400$var$root = (0, ($parcel$interopDefault($g36G4$reactdomclient))).createRoot(document.getElementById("root"));
+$4fa36e821943b400$var$root.render(/*#__PURE__*/ (0, $g36G4$reactjsxruntime.jsx)((0, $86170abfc0122593$export$2e2bcd8739ae039), {}));
+
+
+//# sourceMappingURL=index.js.map
