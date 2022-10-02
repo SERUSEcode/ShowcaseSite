36822df9392f3410ae5befde412acb27bf94a42d
Kevin U <kevin@ullbrandt.com>
2022-10-02 17:12:45 +0200

    push up build

--- React/dist/index.js
+++ React/dist/index.js
@@ -2,10 +2,12 @@ require("./index.css");
 var $gXNCa$reactjsxruntime = require("react/jsx-runtime");
 require("react");
 var $gXNCa$reactdomclient = require("react-dom/client");
+var $gXNCa$reactrouterdom = require("react-router-dom");
 
 function $parcel$interopDefault(a) {
   return a && a.__esModule ? a.default : a;
 }
+
 
 
 
@@ -48,15 +50,166 @@ const $72b69400ac40fd7f$var$AboutMe = ()=>{
 var $72b69400ac40fd7f$export$2e2bcd8739ae039 = $72b69400ac40fd7f$var$AboutMe;
 
 
+
+
+var $5c41b35caffe9a24$exports = {};
+$5c41b35caffe9a24$exports = new URL("house-door.dc56e803.svg", "file:" + __filename).toString();
+
+
+var $f9b3bc35284a6ebb$exports = {};
+$f9b3bc35284a6ebb$exports = new URL("house-door-fill.fa6f3a91.svg", "file:" + __filename).toString();
+
+
+var $0a43057de4c0cb22$exports = {};
+$0a43057de4c0cb22$exports = new URL("box.2aceffa7.svg", "file:" + __filename).toString();
+
+
+var $5270e77b711d6c68$exports = {};
+$5270e77b711d6c68$exports = new URL("box-fill.e009432b.svg", "file:" + __filename).toString();
+
+
+var $628767407bd31d2e$exports = {};
+$628767407bd31d2e$exports = new URL("journals.2fbb5ad4.svg", "file:" + __filename).toString();
+
+
+const $34251def6ce61c5b$var$Navbar = ()=>{
+    return /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsxs)("div", {
+        className: "navbar-container",
+        children: [
+            /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)("a", {
+                href: "/",
+                children: /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsxs)("div", {
+                    href: "/",
+                    className: "navbar-box",
+                    children: [
+                        /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)("img", {
+                            src: (0, (/*@__PURE__*/$parcel$interopDefault($5c41b35caffe9a24$exports))),
+                            alt: "Bootstrap",
+                            width: 50
+                        }),
+                        /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)("p", {
+                            children: "Home"
+                        })
+                    ]
+                })
+            }),
+            /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)("a", {
+                href: "project",
+                children: /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsxs)("div", {
+                    className: "navbar-box",
+                    children: [
+                        /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)("img", {
+                            src: (0, (/*@__PURE__*/$parcel$interopDefault($0a43057de4c0cb22$exports))),
+                            alt: "Bootstrap",
+                            width: 50
+                        }),
+                        /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)("p", {
+                            children: "Project"
+                        })
+                    ]
+                })
+            }),
+            /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)("a", {
+                href: "journal",
+                children: /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsxs)("div", {
+                    className: "navbar-box",
+                    children: [
+                        /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)("img", {
+                            src: (0, (/*@__PURE__*/$parcel$interopDefault($628767407bd31d2e$exports))),
+                            alt: "Bootstrap",
+                            width: 50
+                        }),
+                        /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)("p", {
+                            children: "Journal"
+                        })
+                    ]
+                })
+            })
+        ]
+    });
+};
+var $34251def6ce61c5b$export$2e2bcd8739ae039 = $34251def6ce61c5b$var$Navbar;
+
+
+
+
+var $aed9b7eca3df1294$exports = {};
+$aed9b7eca3df1294$exports = JSON.parse('[{"id":1,"name":"test","description":"Detta project var bra f\xf6r att. Detta gick d\xe5ligt f\xf6r att... Jag l\xe4rde mig det h\xe4r tack vare. ","time":20,"language":"HTML, CSS, Java"},{"id":2,"name":"kurt","description":"Detta project var bra f\xf6r att. Detta gick d\xe5ligt f\xf6r att... Jag l\xe4rde mig det h\xe4r tack vare. ","time":40,"language":"HTML, R, Phyton"},{"id":2,"name":"kurt","description":"Detta project var bra f\xf6r att. Detta gick d\xe5ligt f\xf6r att... Jag l\xe4rde mig det h\xe4r tack vare. ","time":40,"language":"HTML, R, Phyton"}]');
+
+
+const $b6886b9213eeabc6$var$Project = ()=>{
+    return /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsxs)("div", {
+        className: "container",
+        children: [
+            /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)("div", {
+                className: "title",
+                children: /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)("p", {
+                    children: "My projects"
+                })
+            }),
+            /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)("div", {
+                className: "standardtext",
+                children: /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)("p", {
+                    children: "H\xe4r kan du kolla igenom alla mina projekt som jag har gjort under skolg\xe5ngen eller under min fritid. Klicka p\xe5 korten f\xf6r att veta mera"
+                })
+            }),
+            (0, (/*@__PURE__*/$parcel$interopDefault($aed9b7eca3df1294$exports))).map((project, i)=>/*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsxs)("div", {
+                    className: "project-container",
+                    children: [
+                        /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)("div", {
+                            className: "title",
+                            children: project.name
+                        }),
+                        /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsxs)("div", {
+                            className: "standardtext",
+                            children: [
+                                /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)("p", {
+                                    children: project.description
+                                }),
+                                /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)("p", {
+                                    children: project.time
+                                }),
+                                /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)("p", {
+                                    children: project.language
+                                })
+                            ]
+                        })
+                    ]
+                })),
+            /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)("div", {
+                className: "scrollFed"
+            })
+        ]
+    });
+};
+var $b6886b9213eeabc6$export$2e2bcd8739ae039 = $b6886b9213eeabc6$var$Project;
+
+
 const $86170abfc0122593$var$App = ()=>{
     return /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsxs)("div", {
         children: [
-            /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)((0, $75292ee8ae83355f$export$2e2bcd8739ae039), {
-                children: /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)("h1", {
-                    children: "Hall\xe5 d\xe4r v\xe4rlden"
+            /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)((0, $gXNCa$reactrouterdom.BrowserRouter), {
+                children: /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsxs)((0, $gXNCa$reactrouterdom.Routes), {
+                    children: [
+                        /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)((0, $gXNCa$reactrouterdom.Route), {
+                            exact: true,
+                            path: "/",
+                            element: /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsxs)((0, $gXNCa$reactjsxruntime.Fragment), {
+                                children: [
+                                    /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)((0, $75292ee8ae83355f$export$2e2bcd8739ae039), {}),
+                                    /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)((0, $72b69400ac40fd7f$export$2e2bcd8739ae039), {})
+                                ]
+                            })
+                        }),
+                        /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)((0, $gXNCa$reactrouterdom.Route), {
+                            exact: true,
+                            path: "/project",
+                            element: /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)((0, $b6886b9213eeabc6$export$2e2bcd8739ae039), {})
+                        })
+                    ]
                 })
             }),
-            /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)((0, $72b69400ac40fd7f$export$2e2bcd8739ae039), {})
+            /*#__PURE__*/ (0, $gXNCa$reactjsxruntime.jsx)((0, $34251def6ce61c5b$export$2e2bcd8739ae039), {})
         ]
     });
 };
